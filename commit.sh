d3909a4dc585d4cb3dde49cb8668ea0663ec7afa
tim <tim@timdev.de>
2022-07-29 17:55:29 +0200

    feat(dev): add breaking change option to commit.sh

--- commit.sh
+++ commit.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 TYPE=$(gum choose "fix" "feat" "docs" "style" "refactor" "test" "chore" "revert")
 SCOPE=$(gum input --placeholder "scope")
+BREAKING_CHANGE=$(gum confirm -n "Breaking change?" && printf "!")
 
 # Since the scope is optional, wrap it in parentheses if it has a value.
 [[ -n "$SCOPE" ]] && SCOPE="($SCOPE)"
 
 # Pre-populate the input with the type(scope): so that the user may change it
-SUMMARY=$(gum input --value "$TYPE$SCOPE: " --placeholder "Summary of this change")
+SUMMARY=$(gum input --value "$TYPE$SCOPE$BREAKING_CHANGE: " --placeholder "Summary of this change")
 DESCRIPTION=$(gum write --placeholder "Details of this change")
 
 # Commit these changes
