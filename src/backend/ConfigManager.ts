4df22fbb773e732e0c1d3e8bcfc1e5e967c3cb40
tim <tim@timdev.de>
2022-07-31 20:13:13 +0200

    fix(backend): make ConfigManager accessible to every component

--- src/backend/ConfigManager.ts
+++ src/backend/ConfigManager.ts
@@ -6,47 +6,67 @@ import * as path from "path";
 /**
  * A class for managing the LocalTube config
  */
-class ConfigManager {
+export class ConfigManager {
 
-    private configPath: PathLike;
-    private config: MainConfig;
+    private static configPath: PathLike;
+    private static config: MainConfig;
 
     /**
      * Instantiate a config manager that manages the config at a given location
      * @param configPath {string} The path to the localtube.conf.json file
      */
-    constructor(configPath: PathLike) {
-        this.configPath = configPath;
+    private constructor() { }
+
+    /**
+     * Set the path to the config to operate on
+     * @param path {PathLike} Absolute file path of the config
+     */
+    public static setConfigPath(path: PathLike) {
+        ConfigManager.configPath = path;
     }
 
     /**
      * Create a new config if absent, othewise
      * load the contents of the config file into memory
      */
-    loadConfig() {
+    public static loadConfig() {
+        // check if path is set
+        if (!ConfigManager.configPath) throw new Error('Config path not set. Set the path using `ConfigManager.setConfigPath(path: PathLike)`');
+
         // check if config file exists
-        if (!fs.existsSync(this.configPath)) {
+        if (!fs.existsSync(ConfigManager.configPath)) {
             // file does not exist
-            
+
             // create config directory
-            fs.mkdirSync(path.dirname(this.configPath.toString()), { recursive: true });
-            
+            fs.mkdirSync(path.dirname(ConfigManager.configPath.toString()), { recursive: true });
+
             // instantiate empty config object
-            this.config = { shows: [] } as MainConfig;
+            ConfigManager.config = { shows: [] } as MainConfig;
         } else {
             // file does exist
 
             // read file into JSON object
-            this.config = JSON.parse(fs.readFileSync(this.configPath).toString());
+            ConfigManager.config = JSON.parse(fs.readFileSync(ConfigManager.configPath).toString());
         }
+    }
+
+    /**
+     * Write the current config object to disk
+     */
+    public static saveConfig() {
+        // check if path is set
+        if (!ConfigManager.configPath) throw new Error('Config path not set. Set the path using `ConfigManager.setConfigPath(path: PathLike)`');
+        
+        // write serialized JSON object to disk
+        fs.writeFileSync(ConfigManager.configPath, JSON.stringify(ConfigManager.config));
     }
 
     /**
      * Get the config object
      * @returns {MainConfig} The loaded config object
      */
-    public getConfig(): MainConfig {
-        return this.config;
+    public static getConfig(): MainConfig {
+        return ConfigManager.config;
     }
 
 }
