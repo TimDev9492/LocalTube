e8b557484db8936f7ef18b7019df86c219608204
tim <tim@timdev.de>
2022-07-31 20:23:43 +0200

    feat(api): add api route to get database state

--- src/api/contextBridgeBindings.ts
+++ src/api/contextBridgeBindings.ts
@@ -1,13 +1,15 @@
 import { contextBridge, ipcMain, ipcRenderer } from "electron";
 import { PathLike } from "original-fs";
-import { handleReadFile } from "./implementation";
+import { LocalTubeDatabase } from "../backend/structure";
+import { handleGetDatabase, handleReadFile } from "./implementation";
 import { LocalTubeAPI } from "./LocalTubeAPI";
-
 
 contextBridge.exposeInMainWorld('localtubeAPI', {
     readFile: (path: PathLike): Promise<string> => ipcRenderer.invoke('fs:readFile', path),
+    getDatabase: (): Promise<LocalTubeDatabase> => ipcRenderer.invoke('db:getDatabase'),
 } as LocalTubeAPI);
 
 export const initializeBindings: Function = (): void => {
     ipcMain.handle('fs:readFile', handleReadFile);
+    ipcMain.handle('db:getDatabase', handleGetDatabase);
 }
--- src/api/implementation.ts
+++ src/api/implementation.ts
@@ -1,7 +1,13 @@
 import { PathLike } from "original-fs";
 import * as fs from "fs";
 import { IpcMainEvent } from "electron";
+import { LocalTubeDatabase } from "../backend/structure";
+import { DatabaseManager } from "../backend/DatabaseManager";
 
 export function handleReadFile(event: IpcMainEvent, path: PathLike): string {
     return fs.readFileSync(path, { encoding: 'utf-8' });
+}
+
+export function handleGetDatabase(event: IpcMainEvent): LocalTubeDatabase {
+    return DatabaseManager.getDatabase();
 }
