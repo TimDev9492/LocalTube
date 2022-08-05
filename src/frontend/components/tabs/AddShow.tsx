d9fb34ea8d95575df80f647e7d89fc4955f62a02
tim <tim@timdev.de>
2022-08-05 13:54:23 +0200

    refactor(frontend): fix syntax error

--- src/frontend/components/tabs/AddShow.tsx
+++ src/frontend/components/tabs/AddShow.tsx
@@ -50,10 +50,6 @@ export function AddShow(): JSX.Element {
     }
 
     React.useEffect(() => {
-        console.log(regExtract);
-    }, [regExtract]);
-
-    React.useEffect(() => {
         if (!dirPath.length) {
             setIsValidPath(false);
             return;
@@ -130,7 +126,7 @@ export function AddShow(): JSX.Element {
                     <div className="-ml-2 relative">
                         <svg onMouseEnter={() => setConventionalShowTooltip(true)} onMouseLeave={() => setConventionalShowTooltip(false)} fill="currentColor" className="cursor-pointer w-5 h-5 fill-slate-400 right-2 bottom-3" viewBox="0 0 36 36">
                             <path d="M18,2A16,16,0,1,0,34,18,16,16,0,0,0,18,2Zm-.22,25.85a1.65,1.65,0,1,1,1.65-1.65A1.65,1.65,0,0,1,17.78,27.85Zm1.37-8.06v1.72a1.37,1.37,0,0,1-1.3,1.36h-.11a1.34,1.34,0,0,1-1.39-1.3c0-.44,0-2.76,0-2.76a1.19,1.19,0,0,1,1.12-1.31c1.57-.12,4.18-.7,4.18-3.25,0-1.83-1.41-3.07-3.43-3.07a5.31,5.31,0,0,0-4,1.92,1.36,1.36,0,0,1-2.35-.9,1.43,1.43,0,0,1,.43-1,7.77,7.77,0,0,1,6-2.69c3.7,0,6.28,2.3,6.28,5.6C24.58,17.16,22.61,19.2,19.15,19.79Z"></path>
-                            <rect x="0" y="0" width="36" height="36" fill-opacity="0" />
+                            <rect x="0" y="0" width="36" height="36" fillOpacity="0" />
                         </svg>
                         <div className={["px-1 py-2 bg-opacity-80 rounded-md bg-slate-700 absolute w-48 text-center bottom-full left-1/2 -translate-x-1/2 -translate-y-1 text-xs transition-opacity duration-300", conventionalShowTooltip ? "opacity-100" : "opacity-0"].join(' ')}>
                             Whether this show consists of seasons and episodes
